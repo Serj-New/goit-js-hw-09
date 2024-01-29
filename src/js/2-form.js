aecdc01c22a853c897d0a245357a8a2acb677908
Sergey Novikov <69253755+Serj-New@users.noreply.github.com>
2024-01-30 00:45:32 +0200

    HW-09_2 done

---
+++ src/js/2-form.js
@@ -0,0 +1,64 @@
+'use strict';
+
+const STORAGE_KEY = 'feedback-form-state';
+
+const form = document.querySelector('.feedback-form');
+const textarea = form.querySelector('textarea');
+
+form.addEventListener('input', onFormInput);
+form.addEventListener('submit', onFormSubmit);
+
+function onFormInput() {
+    const email = form.elements.email.value.trim();
+    const message = form.elements.message.value.trim();
+
+    const data = {
+        email,
+        message,
+    };
+
+    saveToLS(STORAGE_KEY, data);
+}
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+
+    const email = form.elements.email.value;
+    const message = form.elements.message.value;
+
+    const data = {
+        email,
+        message,
+    };
+
+    console.log(data);
+
+    localStorage.removeItem(STORAGE_KEY);
+
+    form.reset();
+}
+
+function saveToLS(key, value) {
+    const archive = JSON.stringify(value);
+    localStorage.setItem(key, archive);
+
+}
+
+function loadFromLS(key) {
+    const archive = localStorage.getItem(key);
+
+    try {
+        return JSON.parse(archive);
+    } catch {
+        return archive;
+    }
+}
+
+function init() {
+    const data = loadFromLS(STORAGE_KEY) || {};
+
+    form.elements.email.value = data.email || '';
+    form.elements.message.value = data.message || '';
+}
+
+init();
